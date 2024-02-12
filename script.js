7d8dc27f864b5a4f6de3640ede9e90b0b71a3839
Fatima Panhwar <153510549+fatimapanhwar@users.noreply.github.com>
2024-02-12 22:00:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+      let btn = document.querySelector('button');
+      let glow = document.querySelector('.glow');
+      btn.addEventListener("click", ()=>{
+            glow.classList.toggle('go'); 
+        })
